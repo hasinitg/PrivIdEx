bbd4ac33fb87fc4889c34702d56ddac9ff7557b9
hasinitg <hasi7786@gmail.com>
2017-09-17 11:34:30 -0400

    minor comment

--- PrivIdExChaincode/test/handshakePhase_test.go
+++ PrivIdExChaincode/test/handshakePhase_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 )
 
+//This is supposed to test the methods of the chaincode in handshake phase. However, it is not completed yet.
 func Test(t *testing.T) {
 	//create a dummy handshake message
 	initHandshakeMessage := handshake.HandshakeRecord{"tr1", "c1", "u1", "p1",
@@ -19,6 +20,13 @@ func Test(t *testing.T) {
 	//handshake.InitHandshake(shim.ChaincodeStub{})
 
 	if err==nil{
+		encodedMsgStr := string(encodedMsg)
 		fmt.Println(string(encodedMsg))
+		var handshakeRecord handshake.HandshakeRecord
+
+		json.Unmarshal([]byte(encodedMsgStr), &handshakeRecord)
+		fmt.Println(handshakeRecord.ConsumerID)
 	}
+
+
 }
